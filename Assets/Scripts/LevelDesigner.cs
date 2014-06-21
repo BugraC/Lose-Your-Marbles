70a39a2b6daa01ea989c0c70a4f3215ed9a7320a
Bugra Cuhadaroglu <bugracuhadaroglu@hotmail.com>
2014-06-20 22:57:04 -0400

    Created the project.
    Created the project.

---
+++ Assets/Scripts/LevelDesigner.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//The way this level designer works is that
+//Splits the screen into squares and put each item into
+//those squares randomly.
+public class LevelDesigner : MonoBehaviour
+{
+		//Let's create the objects in a static way
+		//So we don't have to worry about the reflection and texture setup.
+		//There might be a better solution but I believe this is the best for me.
+		public GameObject marble1;
+		public GameObject marble2;
+		public GameObject marble3;
+		public GameObject marble4;
+		//Here our screen margins for the setup:
+		public int topMargin = 40;
+		public int bottomMargin = 40;
+		public int leftMargin = 60;
+		public int rightMargin = 40;
+		public int squareSize = 40;
+		public float scaleSize = 0.8f;
+		//This is the floor Y value itself. If you increase this and the objects will on the air. The opposite will be underground.
+		public float constantFloorYCordinate = 0.1274259f;
+		private Random rnd = new Random ();
+		// Use this for initialization
+		void Start ()
+		{
+				//Ok let's create our objects:
+				GameObject marble = null;
+				for (int i = leftMargin; i < Camera.main.pixelWidth - rightMargin; i+= squareSize) {
+						for (int j = topMargin; j< Camera.main.pixelHeight - bottomMargin; j+= squareSize) {
+								switch (Random.Range (1, 5)) {
+								case 1:
+										marble = (GameObject)GameObject.Instantiate (marble1);
+										break;
+								case 2:
+										marble = (GameObject)GameObject.Instantiate (marble2);
+										break;
+								case 3:
+										marble = (GameObject)GameObject.Instantiate (marble3);
+										break;
+								case 4:
+										marble = (GameObject)GameObject.Instantiate (marble4);
+										break;
+								default:
+										break;
+								}
+
+								if(marble != null)
+								{
+									//Let's reduce the size of the elements. They are too big.
+									marble.transform.localScale *= scaleSize;
+									marble.transform.position = Camera.main.ScreenToWorldPoint( new Vector3(i,j,0));
+									//Let's set the y ordinate in real world:
+									marble.transform.position = new Vector3(marble.transform.position.x,constantFloorYCordinate,marble.transform.position.z);
+								}
+
+						}
+				}
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+		private Vector3 startPos;
+		private Vector3 direction;
+		private bool directionChosen;
+		private Vector3 gameObjectStartPosition;
+		public int speed = 100;
+		// This value calibrates the touch range of the object. If you give this range a big value then one touch can choose two marbles at one touch began action which we don't want that.
+		public int touchSensitivity = 20;
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+#if DEBUG
+	void OnGUI(){
+		//This is for debug purposes:
+		//GUI.Label (new Rect (0, 0, 300, 300), startPos.x.ToString () + " " + startPos.y.ToString () + " " +startPos.z.ToString () + " "+ direction.x + " " + direction.y +" " + direction.z);
+		//GUI.Label (new Rect (0, 0, 300, 300), message);
+	}
+#endif
+		// Update is called once per frame
+		void Update ()
+		{
+				// Track a single touch as a direction control.
+				if (Input.touchCount > 0) {
+						var touch = Input.GetTouch (0);
+			
+						// Handle finger movements based on touch phase.
+						switch (touch.phase) {
+						// Record initial touch position.
+						case TouchPhase.Began:
+								startPos = Camera.main.ScreenToWorldPoint (new Vector3 (touch.position.x, touch.position.y, 1.273157f));
+								
+								gameObjectStartPosition = gameObject.transform.position;
+								//Debug.Log (Camera.main.WorldToScreenPoint (gameObject.transform.position).x  + ":" +  touch.position.x);
+								
+								directionChosen = (touch.position.x >= Camera.main.WorldToScreenPoint (gameObject.transform.position).x -touchSensitivity
+				                   && touch.position.x <= Camera.main.WorldToScreenPoint (gameObject.transform.position).x+touchSensitivity);
+								
+								break;
+						// Determine direction by comparing the current touch
+						// position with the initial one.
+						case TouchPhase.Moved:
+								if (directionChosen) {
+										direction = Camera.main.ScreenToWorldPoint (new Vector3 (touch.position.x, touch.position.y, 1.273157f)) - startPos;
+										gameObject.transform.position = new Vector3 (gameObject.transform.position.x, gameObject.transform.position.y, gameObjectStartPosition.z + direction.z);
+										gameObject.transform.Rotate (touch.deltaPosition.y * speed * Time.deltaTime, 0, 0, Space.World);
+										//Debug.Log (touch.deltaPosition.y);
+								}
+								break;
+						// Report that a direction has been chosen when the finger is lifted.
+						case TouchPhase.Ended:
+								if (directionChosen) {
+									directionChosen = true;
+									gameObjectStartPosition = gameObject.transform.position;
+								}
+								break;
+						}
+				}
+		}
+}
